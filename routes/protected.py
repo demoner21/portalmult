7220345824a015a6c2166797ec09d58fafdf4aad
demoner21 <a.tranhago@gmail.com>
2025-03-19 10:07:21 -0300

    .

--- routes/protected.py
+++
@@ -1,8 +0,0 @@
-from fastapi import Depends, FastAPI
-from security import get_current_user, TokenData
-
-app = FastAPI()
-
-@app.get("/protected")
-async def protected_route(current_user: TokenData = Depends(get_current_user)):
-    return {"message": f"Hello, {current_user.email}!"}
