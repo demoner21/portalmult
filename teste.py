69bfe0fe871c02725ea1ff7f133c96752f5e517a
demoner21 <a.tranhago@gmail.com>
2025-04-03 09:11:24 -0300

    deleted: remove the file

--- teste.py
+++
@@ -1,6 +0,0 @@
-from database.database import get_password_hash, verify_password
-
-senha = "Senhazero1!"
-hash_novo = get_password_hash(senha)  # Agora deve funcionar
-print(f"Hash gerado: {hash_novo}")
-print(f"Verificação: {verify_password(senha, hash_novo)}")  # Deve retornar True
